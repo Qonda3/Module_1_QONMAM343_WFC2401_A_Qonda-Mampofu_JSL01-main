ff4319d2f44fb8e2894d2c8f83899fa8b87a3138
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-03-15 04:02:42 +0200

    validate syntax

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
-
+    let compare = /^pet_[\p{Nd}]+\p{L}+$/u;
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
+        if (compare.test(input)){
+            result = "Valid syntax";
+        } 
+        else{
+            result = "Invalid syntax";
+        }
             document.getElementById('result').innerText = result;
 }
 
